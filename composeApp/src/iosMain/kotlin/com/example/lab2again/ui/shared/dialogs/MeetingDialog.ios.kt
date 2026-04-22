4e8d2c959a1496bfa3cba90e919967b68aeabfee
Fizzy <maidaniuk.vladyslav@chnu.edu.ua>
2026-04-22 11:02:22 +0300

    lab5

---
+++ composeApp/src/iosMain/kotlin/com/example/lab2again/ui/shared/dialogs/MeetingDialog.ios.kt
@@ -0,0 +1,15 @@
+package com.example.lab2again.ui.shared.dialogs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Dialog
+
+@Composable
+actual fun MeetingDialogWrapper(
+    onDismiss: () -> Unit,
+    content: @Composable (() -> Unit)
+) {
+    Dialog(
+        onDismissRequest = onDismiss) {
+        content()
+    }
+}
--- composeApp/src/webMain/kotlin/com/example/lab2again/main.kt
+++ composeApp/src/webMain/kotlin/com/example/lab2again/main.kt
@@ -2,10 +2,11 @@ package com.example.lab2again
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import com.example.lab2again.ui.shared_mobile.main.MainScreen
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport {
-        App()
+        MainScreen()
     }
 }
